d943958032f46cc2acac4f2351ca3fe9fa4b80f9
Gabriel Matos <gmatosberger@gmail.com>
2026-09-15 15:12:26 -0300

    Ajusta conexao do banco com dotenv e tratamento de URL

--- database.py
+++ database.py
@@ -1,10 +1,13 @@
 import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
+load_dotenv()
+
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
+if DATABASE_URL.startswith("postgres://"):
     DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
 engine = create_engine(DATABASE_URL)
